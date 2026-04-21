1714c23416824b2464d0448e758fa90dbf8b17d7
hao Sun <sun509579@gmail.com>
2026-04-21 20:16:04 +0800

    dataloder

--- Prepare_data.py
+++ Prepare_data.py
@@ -1,19 +1,14 @@
 import numpy as np
 import torch
-
-xy = np.loadtxt('diabetes.csv.gz', delimiter=',', dtype=np.float32)
-x_data = torch.from_numpy(xy[:, :-1])
-y_data = torch.from_numpy(xy[:, [-1]])
+from torch.utils.data import Dataset  #Dataset是一个抽象类，只能继承不能实例化
+from torch.utils.data import DataLoader  #可以实例化
 
 class Model(torch.nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.linear1 = torch.nn.Linear(8, 16)
-        self.linear2 = torch.nn.Linear(16, 8)
-        self.linear3 = torch.nn.Linear(8, 1)
-        # self.linear1 = torch.nn.Linear(8, 6)
-        # self.linear2 = torch.nn.Linear(6, 4)
-        # self.linear3 = torch.nn.Linear(4, 1)
+        self.linear1 = torch.nn.Linear(8, 6)
+        self.linear2 = torch.nn.Linear(6, 4)
+        self.linear3 = torch.nn.Linear(4, 1)
         self.activate = torch.nn.ReLU()
     def forward(self, x):
         x = self.activate(self.linear1(x))
@@ -21,20 +16,36 @@ class Model(torch.nn.Module):
         x = torch.sigmoid(self.linear3(x))
         return x
 
-model = Model()
-criterion = torch.nn.BCELoss(reduction='mean')
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+class DiabetesDataset(Dataset):
+    def __init__(self, file_path):
+        xy = np.loadtxt(file_path, delimiter=',', dtype=np.float32)
+        self.x_data = torch.from_numpy(xy[:, :-1])  #前8列是特征
+        self.y_data = torch.from_numpy(xy[:, [-1]])  #最后一列是标签
+        self.len = self.y_data.shape[0]  #样本数量
 
-for epoch in range(1000):
+    def __getitem__(self, index):  #索引访问
+        return self.x_data[index], self.y_data[index]
 
-    #forward
-    y_pred = model(x_data)
-    loss = criterion(y_pred, y_data)
-    print(epoch, loss.item())
+    def __len__(self):    #获取数据集大小
+        return self.len
+    
+if __name__ == '__main__':
+    # 确保 diabetes.csv.gz 文件在当前目录，否则使用绝对路径
+    dataset = DiabetesDataset('diabetes.csv.gz')  # 实例化数据集对象
+    train_loader = DataLoader(dataset=dataset, batch_size=32, shuffle=True, num_workers=2)
 
-    #backward
-    optimizer.zero_grad()
-    loss.backward()
+    model = Model()
+    criterion = torch.nn.BCELoss(reduction='mean')
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
-    #update
-    optimizer.step()
+    for epoch in range(100):
+        for i, (x_batch, y_batch) in enumerate(train_loader):
+            inputs = x_batch
+            labels = y_batch
+            y_pred = model(inputs)
+            loss = criterion(y_pred, labels)
+            print(epoch, i, loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
